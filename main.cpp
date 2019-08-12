deb87ec3f0a6a017067a4420be78f4292aaad02f
Maxim Moroz <manager@fmdepo.net>
2019-08-12 20:45:16 +0300

    graded

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
 
 int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int64_t> temperatures(n);
+    int64_t sum = 0;
+    for (auto & temperature : temperatures) {
+        cin >> temperature;
+        sum += temperature;
+    }
+
+    int64_t average = sum / n;
+
+    vector<size_t> result_indices;
+    for (size_t i = 0; i < temperatures.size(); ++i) {
+        if (temperatures[i] > average) {
+            result_indices.push_back(i);
+        }
+    }
+
+    cout << result_indices.size() << endl;
+    copy(result_indices.cbegin(), result_indices.cend(), ostream_iterator<size_t>(cout, " "));
+    cout << endl;
+
     return 0;
 }
