480319b0c03258f5a9e980e8bfca7964cc21941d
Maxim Moroz <manager@fmdepo.net>
2019-08-14 07:52:46 +0300

    graded

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    unsigned n, density;
+    cin >> n >> density;
+
+    uint64_t mass = 0;
+    for(unsigned i = 0; i < n; ++i) {
+        unsigned width, height, depth;
+        cin >> width >> height >> depth;
+        mass += static_cast<uint64_t >(density) * width * height * depth;
+    }
+
+    cout << mass << endl;
     return 0;
 }
