b6ad86fccd450169ab3832c0accbd359962d538c
Maxim Moroz <manager@fmdepo.net>
2019-08-17 17:39:14 +0300

    graded

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+template <typename K, typename V>
+V& GetRefStrict(map<K, V>& col, const K& k)
+{
+    try {
+        return col.at(k);
+    }
+    catch (const out_of_range& e) {
+        throw runtime_error("Key error");
+    }
+}
 int main() {
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl; // выведет newvalue
     return 0;
 }
