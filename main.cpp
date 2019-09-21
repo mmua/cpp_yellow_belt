cf63a346ba652b565b38c673f4a2fa689b4ef7a5
Maxim Moroz <manager@fmdepo.net>
2019-09-21 22:39:01 +0300

    Revert "graded"
    This reverts commit ad76d3015dd8bd88867bae6fb235b4935bb1e6f8.

--- main.cpp
+++ main.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
-#include <set>
-#include <algorithm>
 
 using namespace std;
-set<int>::const_iterator FindNearestElement(
-        const set<int>& numbers,
-        int border);
-// set<int>::const_iterator —
-// тип итераторов для константного множества целых чисел
-
-set<int>::const_iterator FindNearestElement(
-        const set<int>& numbers,
-        int border)
-{
-    auto p = numbers.lower_bound(border);
-
-    if(p == begin(numbers)) return p;
-    if(p == end(numbers)) return prev(p);
-
-    if(abs(*prev(p) - border) > abs(*p - border)) {
-        return p;
-    } else {
-        return prev(p);
-    }
-}
 
 int main() {
-    set<int> numbers = {1, 4, 6};
-    cout <<
-         *FindNearestElement(numbers, 0) << " " <<
-         *FindNearestElement(numbers, 3) << " " <<
-         *FindNearestElement(numbers, 5) << " " <<
-         *FindNearestElement(numbers, 6) << " " <<
-         *FindNearestElement(numbers, 100) << endl;
-
-    set<int> empty_set;
-
-    cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
     return 0;
 }
