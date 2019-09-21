3864a88a29e7255867e3d6b11bcfefa4202f0c0b
Maxim Moroz <manager@fmdepo.net>
2019-09-21 22:00:37 +0300

    graded

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iterator>
 
 using namespace std;
 
 int main() {
+    size_t n;
+    vector<int> nums = {};
+
+    cin >> n;
+    nums.resize(n);
+
+    for(size_t i = n; i > 0; --i) {
+        nums[i - 1] = n - i + 1;
+    }
+    do {
+        copy(begin(nums), end(nums), ostream_iterator<int>(cout, " "));
+        cout << endl;
+    } while (prev_permutation(begin(nums), end(nums)));
     return 0;
 }
