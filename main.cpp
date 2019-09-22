160e6f93b0f1dfb6dd1c3d18045446ad05cf6921
Maxim Moroz <manager@fmdepo.net>
2019-09-22 20:20:56 +0300

    graded

--- main.cpp
+++ main.cpp
@@ -207,6 +207,24 @@ int main() {
     runner.RunTest(TestUnknownFirstName, "Test Unknown First Name");
     runner.RunTest(TestUnknownLastName, "Test Unknown Last Name");
     runner.RunTest(TestFirstNameLastName, "Test First Name Last Name");
+        Person person;
+
+        person.ChangeFirstName(1965, "Polina");
+        person.ChangeLastName(1967, "Sergeeva");
+        for (int year : {1900, 1965, 1990}) {
+            cout << person.GetFullName(year) << endl;
+        }
+
+        person.ChangeFirstName(1970, "Appolinaria");
+        for (int year : {1969, 1970}) {
+            cout << person.GetFullName(year) << endl;
+        }
+
+        person.ChangeLastName(1968, "Volkova");
+        for (int year : {1969, 1970}) {
+            cout << person.GetFullName(year) << endl;
+        }
+
 
     return 0;
 }
