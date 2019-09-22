3141e5ba248e8d54b1a6a9d75ef3319369e24116
Maxim Moroz <manager@fmdepo.net>
2019-09-22 20:50:43 +0300

    Revert "graded"
    This reverts commit ae1671d88787ff499a0577bdfc20257a94a9d8d9.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(
-        RandomIt range_begin, RandomIt range_end,
-        const string& prefix)
-{
-    auto begin = partition_point(range_begin, range_end, [prefix](const string& item) {return item.substr(0, prefix.length()) < prefix;});
-    auto end = partition_point(range_begin, range_end, [prefix](const string& item) {return item.substr(0, prefix.length()) <= prefix;});
-
-    return make_pair(begin, end);
-}
-
 int main() {
-    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
-
-    const auto mo_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-    for (auto it = mo_result.first; it != mo_result.second; ++it) {
-        cout << *it << " ";
-    }
-    cout << endl;
-
-    const auto mt_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " <<
-         (mt_result.second - begin(sorted_strings)) << endl;
-
-    const auto na_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-    cout << (na_result.first - begin(sorted_strings)) << " " <<
-         (na_result.second - begin(sorted_strings)) << endl;
-
     return 0;
 }
